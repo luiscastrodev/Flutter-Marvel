cb1de5d3eb56cc3d0aa1f162bc5d908c15d15da0
luiscastrodev <luisricardodecastro@gmail.com>
2020-04-08 16:13:38 -0300

    alter
    change the placeholder

--- marvel_app/lib/API/api.dart
+++ marvel_app/lib/API/api.dart
@@ -7,8 +7,8 @@ import 'package:http/http.dart' as http;
 class API {
   Future<List<Character>> getCharacterList() async {
     const String url = "http://gateway.marvel.com/v1/public/";
-    const private_key = "893f164706c1a4acca3b4fc26665e4511f442a0e";
-    const String public_key = "4f1afcd3011628ca45df2faeb08d3bd7";
+    const private_key = "your private key from marvel";
+    const String public_key = "your public key from marvel";
 
     var crypto = utf8.encode("1" + private_key + public_key);
     var digest = md5.convert(crypto);
